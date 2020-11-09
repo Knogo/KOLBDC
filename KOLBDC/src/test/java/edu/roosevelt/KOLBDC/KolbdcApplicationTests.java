7e3117a92f0816bbc89d367f06e8a2ede3440ee2
Knogo <57305222+Knogo@users.noreply.github.com>
2020-11-09 10:51:21 -0600

    Add files via upload

---
+++ KOLBDC/src/test/java/edu/roosevelt/KOLBDC/KolbdcApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.roosevelt.KOLBDC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KolbdcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
