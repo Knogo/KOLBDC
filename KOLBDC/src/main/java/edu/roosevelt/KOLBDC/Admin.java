37888124775186b57bab1bd367de937e77c9585f
Level <Level@StarAqua.attlocal.net>
2020-11-15 00:08:53 -0600

    Added Admin and Creator class file Admin has private values

---
+++ KOLBDC/src/main/java/edu/roosevelt/KOLBDC/Admin.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.roosevelt.KOLBDC;
+
+/**
+ *
+ * @author Level
+ */
+public class Admin {
+    private int ID;
+    private String name;
+    private int coins;
+    private String upgrades;
+    private String pass; 
+   
+}
