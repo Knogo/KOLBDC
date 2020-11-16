814049a934b46fde901b24f5b0e1e9ea0b9e5c2d
Knogo <57305222+Knogo@users.noreply.github.com>
2020-11-15 21:49:39 -0600

    Added logic behind running simple endpoints for controller

--- KOLBDC/src/main/java/edu/roosevelt/KOLBDC/Controller.java
+++ KOLBDC/src/main/java/edu/roosevelt/KOLBDC/Controller.java
@@ -5,10 +5,16 @@
  */
 package edu.roosevelt.KOLBDC;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -28,4 +34,67 @@ public class Controller {
 
     @Autowired
     AdminRepository adminDB; //For Creators
+    
+    @GetMapping({"/diver/","/diver/{id}"})
+    public ResponseEntity<List<Diver>> getDivers(@PathVariable(value = "id", required = false) final Integer id) {
+        if (id != null) {
+            if (divDB.existsById(id)) {
+                Diver d = divDB.findByID(id);
+                List<Diver> result = new ArrayList();
+                result.add(d);
+                return new ResponseEntity(result, HttpStatus.OK);
+            } else {
+                return new ResponseEntity("Not found", HttpStatus.NOT_FOUND);
+            }
+        } else {
+            List<Diver> result = (List<Diver>)divDB.findAll();
+            if (result == null || result.isEmpty()) {
+                return new ResponseEntity(result, HttpStatus.NO_CONTENT);
+            } else {
+                return new ResponseEntity(result, HttpStatus.OK);
+            }
+        }
+    }
+    
+    @GetMapping({"/creator/","/creator/{id}"})
+    public ResponseEntity<List<Creator>> getCreators(@PathVariable(value = "id", required = false) final Integer id) {
+        if (id != null) {
+            if (creDB.existsById(id)) {
+                Creator c = creDB.findByID(id);
+                List<Creator> result = new ArrayList();
+                result.add(c);
+                return new ResponseEntity(result, HttpStatus.OK);
+            } else {
+                return new ResponseEntity("Not found", HttpStatus.NOT_FOUND);
+            }
+        } else {
+            List<Creator> result = (List<Creator>)creDB.findAll();
+            if (result == null || result.isEmpty()) {
+                return new ResponseEntity(result, HttpStatus.NO_CONTENT);
+            } else {
+                return new ResponseEntity(result, HttpStatus.OK);
+            }
+        }
+    }
+    
+    @GetMapping({"/admin/","/admin/{id}"})
+    public ResponseEntity<List<Admin>> getAdmins(@PathVariable(value = "id", required = false) final Integer id) {
+        if (id != null) {
+            if (adminDB.existsById(id)) {
+                Admin a = adminDB.findByID(id);
+                List<Admin> result = new ArrayList();
+                result.add(a);
+                return new ResponseEntity(result, HttpStatus.OK);
+            } else {
+                return new ResponseEntity("Not found", HttpStatus.NOT_FOUND);
+            }
+        } else {
+            List<Admin> result = (List<Admin>)adminDB.findAll();
+            if (result == null || result.isEmpty()) {
+                return new ResponseEntity(result, HttpStatus.NO_CONTENT);
+            } else {
+                return new ResponseEntity(result, HttpStatus.OK);
+            }
+        }
+    }
 }
