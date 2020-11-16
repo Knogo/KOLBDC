56a384a70df105c9e7fc15653c9280b68475f332
Knogo <57305222+Knogo@users.noreply.github.com>
2020-11-15 21:37:30 -0600

    Admin Attributes have been changed to a bean + insert code

--- KOLBDC/src/main/java/edu/roosevelt/KOLBDC/Admin.java
+++ KOLBDC/src/main/java/edu/roosevelt/KOLBDC/Admin.java
@@ -5,15 +5,69 @@
  */
 package edu.roosevelt.KOLBDC;
 
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  *
  * @author Level
  */
-public class Admin {
+@Entity
+@Table(name = "ADMIN")
+public class Admin implements Serializable {
+    @Id
     private int ID;
     private String name;
-    private int coins;
-    private String upgrades;
-    private String pass; 
-   
+    private String password;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 71 * hash + this.ID;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Admin other = (Admin) obj;
+        if (this.ID != other.ID) {
+            return false;
+        }
+        return true;
+    }
 }
