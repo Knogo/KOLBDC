48dbac1a0299088381918c274e5576199707c5f0
Knogo <57305222+Knogo@users.noreply.github.com>
2020-11-15 21:33:04 -0600

    Diver Attributes have been changed to a bean + insert code

--- KOLBDC/src/main/java/edu/roosevelt/KOLBDC/Diver.java
+++ KOLBDC/src/main/java/edu/roosevelt/KOLBDC/Diver.java
@@ -5,15 +5,100 @@
  */
 package edu.roosevelt.KOLBDC;
 
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  *
  * @author kenom and level
  */
-public class Diver {
+@Entity
+@Table(name = "DIVER")
+public class Diver implements Serializable  {
+    @Id
     private int ID;
     private String name;
+    private String password;
     private int coins;
     private String upgrades;
     private String clears;
-    private String pass;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getCoins() {
+        return coins;
+    }
+
+    public void setCoins(int coins) {
+        this.coins = coins;
+    }
+
+    public String getUpgrades() {
+        return upgrades;
+    }
+
+    public void setUpgrades(String upgrades) {
+        this.upgrades = upgrades;
+    }
+
+    public String getClears() {
+        return clears;
+    }
+
+    public void setClears(String clears) {
+        this.clears = clears;
+    }
+    
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 47 * hash + this.ID;
+        hash = 47 * hash + this.coins;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Diver other = (Diver) obj;
+        if (this.ID != other.ID) {
+            return false;
+        }
+        if (this.coins != other.coins) {
+            return false;
+        }
+        return true;
+    }
 }
